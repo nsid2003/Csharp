fc71f1d536995041496ffb3cc3aa592f595e3d9e
nsid <ismaelngassa730@gmail.com>
2022-12-20 16:28:15 +0100

    mon premier programme C#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Yes nsid!");
+﻿using System; // le fichier source utilise l'espace de nom nommé "system"
+public class Helloworld{ // déclaration d'une classe nommée HElloworld
+
+    public static void main (){ // déclaration d'une méthode statique nommée main
+
+        Console.WriteLine("Yes nsid!"); // permet d'afficher le message Yes nsid
+        Console.ReadLine(); // cette commande permet d'attendre que la touche entré soit tapé avant de continuer l'exe 
+    }
+
+}
